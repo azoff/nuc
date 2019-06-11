8d3f8d3895b0a135326ed46e0a50c73050e2db45
Jonathan Azoff <jon@beomni.com>
2019-06-10 23:39:32 -0700

    one more fix

--- scripts/create-registry-htpasswd.sh
+++ scripts/create-registry-htpasswd.sh
@@ -3,5 +3,5 @@
 set -ex
 
 kubectl create secret generic regcred \
-    --from-file=.dockerconfigjson=~/.docker/config.json \
+    --from-file=.dockerconfigjson=$(echo ~/.docker/config.json) \
     --type=kubernetes.io/dockerconfigjson
