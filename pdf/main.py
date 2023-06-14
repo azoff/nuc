0c8f7311a0e07f888f731853880482739dda0f09
Jonathan Azoff <jon@azof.fr>
2023-06-14 02:06:06 -0700

    use gpt-3.5-turbo

--- pdf/main.py
+++ pdf/main.py
@@ -158,7 +158,7 @@ def create_completions(prompt):
 	openai.api_key = os.environ.get("OPENAI_API_KEY")
 	logging.info(f"Creating completion...")
 	completion = openai.Completion.create(
-		engine="text-davinci-003",
+		engine="gpt-3.5-turbo",
 		prompt=prompt,
 		max_tokens=512,
 		n=1,
